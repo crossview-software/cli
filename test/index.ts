1274f6bcf611538e574683e1d08569c30591d0b7
Liam McAlister <liam@liammcalister.com>
2019-12-26 11:41:31 -0600

    changed test files to typescript with a pretest hook to build

---
+++ test/index.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha"
+import { assert } from "chai"
+
+describe("New initial test", function() {
+	it("should pass", function() {
+		assert.equal(1, 1, "That's really weird...")
+	})
+})
