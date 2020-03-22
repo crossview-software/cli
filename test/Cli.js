19aea5ad57b5f0b98529820c6f66417cf3dda1ad
liammcalister <liam@liammcalister.com>
2020-03-21 19:18:22 -0500

    saving work, added things

---
+++ test/Cli.js
@@ -0,0 +1,7 @@
+// const {describe, it} = require("mocha")
+// const {assert} = require("chai")
+// const {Cli, Flag} = require("../lib/index")
+//
+// describe("@crossview/cli Cli object", function() {
+//
+// })
---
+++ test/Flag.js
@@ -0,0 +1,61 @@
+const {describe, it} = require("mocha")
+const {assert, expect} = require("chai")
+const {Flag} = require("../lib/index")
+
+describe("@crossview/cli Flag class", function() {
+
+    it("should have the correct properties set", function() {
+
+        const flag = new Flag("hello", "h", "Just a test", () => 42)
+
+        expect(flag.flag).to.have.string("hello")
+        expect(flag.shortFlag).to.have.string("h")
+        expect(flag.description).to.have.string("Just a test")
+        expect(flag.handler()).to.equal(42)
+        expect(flag.args).to.be.an("array").that.has.lengthOf(0)
+
+    })
+
+    it("should throw a TypeError if the flag name is not specified", function() {
+
+        expect(() => new Flag()).to.throw(TypeError, "Argument 'flag' must be a valid string")
+
+    })
+
+    it("should add the correct short flag if not defined", function() {
+
+        const flag = new Flag("hello")
+        assert.equal(flag.shortFlag, "h", "shortname is incorrect")
+
+    })
+
+    it("should include a default empty description if the description is undefined", function() {
+
+        const flag = new Flag("hello")
+        expect(flag.description).to.have.string("")
+
+    })
+
+    it("should add strings to the args array when addArg is called", function() {
+
+        const flag = new Flag("hello")
+        flag.addArg("People")
+        flag.addArg("say")
+        flag.addArg("that")
+        flag.addArg("I'm")
+        flag.addArg("the")
+        flag.addArg("best")
+        flag.addArg("boss")
+
+        expect(flag.args)
+            .to.include("People")
+            .to.include("say")
+            .to.include("that")
+            .to.include("I'm")
+            .to.include("the")
+            .to.include("best")
+            .to.include("boss")
+
+    })
+
+})
